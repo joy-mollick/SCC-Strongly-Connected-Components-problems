ab92e852bdb5c61aed258c998968acf50b903f12
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-08-11 07:32:39 -0700

    Create Codeforces 22 E. Scheme.cpp

---
+++ Codeforces 22 E. Scheme.cpp
@@ -0,0 +1,93 @@
+
+/// Time-1290ms
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int mx=100001;
+
+bool visited[mx]={false};
+
+int scc_number[mx];
+
+int connctd_cmp_cnt;
+
+///each connected component's properties (first and second)
+pair<int,int> cmp_pro[mx];
+
+vector<int>adj[mx];
+vector<int>rev[mx];
+
+void dfs(int u)
+{
+    visited[u]=true;
+    vector<int>::iterator it;
+    for(it=adj[u].begin();it!=adj[u].end();it++)
+    {
+        int v=(*it);
+        if(!visited[v])
+        {
+            dfs(v);
+        }
+        else
+        {
+            cmp_pro[connctd_cmp_cnt].second=v;
+        }
+    }
+}
+
+int main()
+{
+    /// directed graph
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        int to;
+        cin >> to;
+        adj[i].push_back(to);
+        rev[to].push_back(i);
+    }
+
+    /// this is for those fact where , a path from a node i goes towards a cycle
+     for (int i = 1; i <= n; ++i)
+    {
+        /// no incoming edge comes to this node
+        if ((rev[i].size())==0)
+        {
+            cmp_pro[connctd_cmp_cnt].first = i;
+            dfs(i);
+            connctd_cmp_cnt++;
+        }
+    }
+
+    /// this is for disconnected cycle (loop)
+     for (int i = 1; i <= n; ++i) {
+        if (!visited[i])
+        {
+            cmp_pro[connctd_cmp_cnt].first = i;
+            dfs(i);
+            connctd_cmp_cnt++;
+        }
+    }
+
+    /// there is only one cycle, so no need to add any edges.
+    if (connctd_cmp_cnt == 1 && cmp_pro[0].first == cmp_pro[0].second) { puts("0");return 0;}
+    vector< pair<int,int> > ans;
+
+    /// connecting consecutive connected component
+    for (int i = 0; i < connctd_cmp_cnt-1; ++i)
+        ans.push_back(make_pair(cmp_pro[i].second, cmp_pro[(i+1)].first));
+
+        /// at last connect first connected and last connected component,which makes total a strongly connected component.
+        ans.push_back(make_pair(cmp_pro[connctd_cmp_cnt-1].second, cmp_pro[0].first));
+
+    cout << (int)(ans.size()) << '\n';
+    for(int y=0;y<(int)(ans.size());y++)
+    {
+       cout<<ans[y].first<<" "<<ans[y].second<<endl;
+    }
+
+    return 0;
+}
